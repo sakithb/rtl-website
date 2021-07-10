1e71a2f6438f69807146e527c63059825ff7e595
Sakith B <pvsakith@gmail.com>
2021-07-10 18:47:13 +0530

    ennm

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,25 +1,26 @@
-const handleSubmit = (e) => {
-  e.preventDefault()
-  let uName = document.getElementById("inputName")
-  let uEmail = document.getElementById("inputEmail")
-  let uSubject = document.getElementById("inputSubject")
-  let uMessage = document.getElementById("inputMessage")
-  let formData = new FormData()
-  formData.append("name", uName.value)
-  formData.append("email", uEmail.value)
-  formData.append("subject", uSubject.value)
-  formData.append("message", uMessage.value)
-  console.log(formData)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Message successfully submitted')).catch((error) =>
-    alert(error))
-}
+// const handleSubmit = (e) => {
+//   e.preventDefault()
+//   let cForm = document.getElementById('contactForm');
+//   // let uName = cForm.getElementById("inputName")
+//   // let uEmail = cForm.getElementById("inputEmail")
+//   // let uSubject = cForm.getElementById("inputSubject")
+//   // let uMessage = cForm.getElementById("inputMessage")
+//   // let formData = new FormData()
+//   // formData.append("name", uName.value)
+//   // formData.append("email", uEmail.value)
+//   // formData.append("subject", uSubject.value)
+//   // formData.append("message", uMessage.value)
+//   console.log(formData)
+//   fetch('/', {
+//     method: 'POST',
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//     body: new URLSearchParams(formData).toString()
+//   }).then(() => console.log('Message successfully submitted')).catch((error) =>
+//     alert(error))
+// }
 
-function enableSubmitBtn() {
-  document.getElementById("contactSubmitBtn").disabled = false;
-}
+// function enableSubmitBtn() {
+//   document.getElementById("contactSubmitBtn").disabled = false;
+// }
 
 document.querySelector("#contactForm").addEventListener("submit", handleSubmit);
