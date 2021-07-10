3041d464e5ab4c0a794a80b6e3eb5b95e668d54a
Sakith B <pvsakith@gmail.com>
2021-07-10 18:15:49 +0530

    idk

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,17 +1,3 @@
-const handleSubmit = (e) => {
-  e.preventDefault()
-  let cForm = document.getElementById('contactForm');
-  let formData = new FormData(cForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Message successfully submitted')).catch((error) =>
-    alert(error))
-}
-
 function enableSubmitBtn() {
   document.getElementById("contactSubmitBtn").disabled = false;
 }
-
-document.querySelector("#contactForm").addEventListener("submit", handleSubmit);
